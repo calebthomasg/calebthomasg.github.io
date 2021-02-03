3ca0fbcb1fb0b7bc442d07c6b4423f5b10e25f86
calebthomasg <calebthomasg@reagan.com>
2021-02-02 19:22:06 -0700

    updates

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,8 +1,4 @@
-function toggleMenu() {
-    document.getElementById("navigation").classList.toggle("hide");
-}
 
-//Today's date display in footer
 const save_date = new Date();
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
