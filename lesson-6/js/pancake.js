3020d30525f1465d494dac54308a70d0ce4e7914
calebthomasg <calebthomasg@reagan.com>
2021-03-12 12:05:43 -0700

    pancake and image resizing

---
+++ lesson-6/js/pancake.js
@@ -0,0 +1,4 @@
+new Date().getDay() == 5
+  ? (document.getElementById("pancake").innerHTML =
+      "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.")
+  : (document.getElementById("pancake").style.display = "block");
