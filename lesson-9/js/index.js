5177544a27ff28923652d662b74bb5e5493bf5ed
calebthomasg <calebthomasg@reagan.com>
2021-03-17 17:24:09 -0600

    index update

--- lesson-9/js/index.js
+++ lesson-9/js/index.js
@@ -1,86 +1,4 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-const TOWNS = [
-	"Fish Haven",
-    "Preston",
-    "Soda Springs"
-];
-
-let townCount = 0;
-
-const renderTownInfo = (infoDiv, value, label) => {
-    let info = document.createElement('div');
-    info.classList.add('info');
-
-    let infoValue = document.createElement('div');
-    infoValue.classList.add('value');
-    infoValue.textContent = value;
-
-    let infoType = document.createElement('div');
-    infoType.classList.add('type');
-    infoType.textContent = label;
-
-    info.appendChild(infoValue);
-    info.appendChild(infoType);
-    infoDiv.appendChild(info);
-};
-
-const renderTownCard = townData => {
-    if (!TOWNS.find(townName => townData.name === townName)) {
-    	return false;
-    }
-    
-    townCount += 1;
-
-    let card = document.createElement('article');
-    card.classList.add('card');
-
-    // Link
-    var link = document.createElement('a');
-
-    // Comment
-    var com = document.createComment(`Card n°${townCount}`);
-    document.getElementById('cards').appendChild(com);
-
-    // card-image
-    let image = document.createElement('div');
-    image.classList.add('card-image');
-    card.appendChild(image);
-    let photoUrl = 'url("img/' + townData.photo + '")'
-    image.style.background = photoUrl;
-
-    // card-title
-    let titleDiv = document.createElement('div');
-    titleDiv.classList.add('card-title');
-    card.appendChild(titleDiv);
-
-    let cardH3 = document.createElement('h3');
-    cardH3.textContent = townData.name;
-    titleDiv.appendChild(cardH3);
-
-    let cardH4 = document.createElement('h4');
-    cardH4.textContent = townData.motto;
-    titleDiv.appendChild(cardH4);
-
-    // card-info
-    let infoDiv = document.createElement('div');
-    infoDiv.classList.add('card-info');
-    card.appendChild(infoDiv);
-
-    renderTownInfo(infoDiv, townData.yearFounded, "Year Founded");
-    renderTownInfo(infoDiv, townData.currentPopulation, "Current Population");
-    renderTownInfo(infoDiv, townData.averageRainfall, "Average Rainfall");
-
-    link.appendChild(card);
-    document.getElementById('cards').appendChild(link);
-
-    if (townData.name == "Fish Haven") {
-        link.setAttribute('href', 'fish-haven.html');
-    } else if (townData.name == "Preston") {
-        link.setAttribute('href', 'preston.html');
-    } else if (townData.name == "Soda Springs") {
-        link.setAttribute('href', 'soda-spring.html');
-    }
-};
 
 fetch(requestURL)
     .then(function (response) {
@@ -91,7 +9,37 @@ fetch(requestURL)
         const towns = jsonObject['towns'];
 
         for (let i = 0; i < towns.length; i++) {
-                renderTownCard(towns[i]);
-            
+            if (i == 2 || i == 0 || i == 6) {
+                let card = document.createElement('div');
+                let info = document.createElement('section');
+                let photo = document.createElement('figure');
+                let name = document.createElement('h2');
+                let motto = document.createElement('h3');
+                let date = document.createElement('p');
+                let people = document.createElement('p');
+                let rain = document.createElement('p');
+                let image = document.createElement('img');
+
+                name.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                date.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                people.textContent = 'Population: ' + towns[i].currentPopulation;
+                rain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+                image.setAttribute('src', 'images/'+towns[i].photo);
+                image.setAttribute('alt', towns[i].name + ' Hometown');
+
+                photo.appendChild(image);
+                card.appendChild(photo);
+
+                info.appendChild(name);
+                info.appendChild(motto);
+                info.appendChild(date);
+                info.appendChild(people);
+                info.appendChild(rain);
+                card.appendChild(info);
+
+                document.getElementById('cards').appendChild(card);
+            }
         }
+
     });
