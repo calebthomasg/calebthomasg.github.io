7afede14300ef41dbbd7bd6779281adbcbf00603
calebthomasg <calebthomasg@reagan.com>
2021-04-01 22:43:15 -0600

    json fix

--- lesson-13/js/directory.js
+++ lesson-13/js/directory.js
@@ -9,33 +9,28 @@ fetch(requestURL)
         const business = jsonObject['business'];
 
         for (let i = 0; i < business.length; i++) {
-            if (i == 0 || i == 1 || i == 2 ) {
+            if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 7 || i == 8) {
                 let card = document.createElement('div');
                 let info = document.createElement('section');
                 let logo = document.createElement('figure');
-                let image = document.createElement('img');
                 let name = document.createElement('h2');
                 let phone = document.createElement('p');
                 let website = document.createElement('p');
-                
+                let image = document.createElement('img');
 
-                
                 name.textContent = business[i].name;
-                image.setAttribute('src', 'images/'+business[i].logo);
-                image.setAttribute('alt', business[i].name + ' logo');
                 phone.textContent = business[i].phone;
                 website.textContent = 'Website: ' + business[i].website;
-                
-                
+                image.setAttribute('src', 'images/'+business[i].logo);
+                image.setAttribute('alt', business[i].name + ' logo');
 
-                info.appendChild(name);
-                card.appendChild(info);
                 logo.appendChild(image);
                 card.appendChild(logo);
+
+                info.appendChild(name);
                 info.appendChild(phone);
                 info.appendChild(website);
-                
-
+                card.appendChild(info);
 
                 document.getElementById('cards').appendChild(card);
             }
