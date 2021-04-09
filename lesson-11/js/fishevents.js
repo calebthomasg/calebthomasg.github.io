cc9d3b47144afda8b79d50cfc654bba2dcb8b2fa
calebthomasg <calebthomasg@reagan.com>
2021-04-08 23:08:11 -0600

    events creation

---
+++ lesson-11/js/fishevents.js
@@ -0,0 +1,32 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+.then(function (response) {
+    return response.json();
+})
+.then(function (jsonObject) {
+    console.table(jsonObject);
+    const towns = jsonObject['towns'];
+
+    for (let i = 0; i < towns.length; i++) {
+        
+        if (i == 2) {
+            let card = document.createElement('div');
+            let info = document.createElement('section');
+            let heading = document.createElement("h2");
+            let event = document.createElement('p');
+
+            heading.textContent = "Upcoming Events";
+            event.textContent = towns[i].events;
+            
+
+            info.appendChild(heading);
+            info.appendChild(event);
+            card.appendChild(info);
+
+            document.getElementById('cards2').appendChild(card);
+        }
+        
+    }
+
+});
--- lesson-11/js/index.js
+++ lesson-11/js/index.js
@@ -43,3 +43,4 @@ fetch(requestURL)
         }
 
     });
+
